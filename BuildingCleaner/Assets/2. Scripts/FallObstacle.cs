d2b42bb4e6c8c7a719cd9c3820558d6639d3cce9
KarbyLee179 <forleer179@gmail.com>
2023-04-26 16:25:15 +0900

    떨어지는 쓰레기들 랜덤 생성 스크립트 완성

--- BuildingCleaner/Assets/2. Scripts/FallObstacle.cs
+++ BuildingCleaner/Assets/2. Scripts/FallObstacle.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class FallObstacle : MonoBehaviour
 {
+    [SerializeField] GameObject imgGameObject;
+    private SpriteRenderer theSR;
+    [SerializeField] Sprite[] garbageImgs;
+    
+
+    [SerializeField] float fallSpeed = 3f;
     
     void Start()
     {
-        
+        theSR = imgGameObject.GetComponent<SpriteRenderer>();
+
+        int garbageNum = Random.Range(0, 3);
+        theSR.sprite = garbageImgs[garbageNum];
     }
 
     
     void Update()
     {
-        
+        transform.Translate(Vector2.down * fallSpeed * Time.deltaTime);
+
+        if(transform.position.y <= -1)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
